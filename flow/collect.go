5eb2f085782726f682382358cc59590669315482
Gavin Kyte <gpkyte@gmail.com>
2021-02-10 23:23:46 -0500

    Compile collect.go
    Behavior fails test, but logically is almost correct
    Several changes are logged below
    
    Removed the collectable type, now just interface{}
    Unexported help function
    Correct Name of FilterFunc in Select Header
    Change Pointer to actual reference
    Move interface type, Collection, to parameters for ImmutableMapReduce()
    All exported functions now have documentation comment
    Add runtime type check before cast within switch block, twice
    Change incorrect `type` keyword to `func` where necessary

--- flow/collect.go
+++ flow/collect.go
@@ -10,100 +10,105 @@ package flow // import "github.com/GPKyte/banter/flow"
 // Where the type ambiguity is at fault.
 // Often it boils down to the interface{} and type checking would happen in the whichAreGood function
 
-
+// Collection can be used with filter operations defined here or elsewhere to collect subsets neatly.
 type Collection interface {
 	Len() int
-	Get(this int) collectable
+	Get(this int) interface{}
 }
 
-type collectable interface {}
-
-func NewGenericCollection(ofThese... []interface{}) GenericCollection {
+// newGenericCollection is simply a helper method to init a GenericCollection
+func newGenericCollection(ofThese ...[]interface{}) GenericCollection {
 	var cap int = 0 // no cap, this is a cap, no cap
-	for each := range ofThese {
+	for _, each := range ofThese {
+		cap += len(each)
+	}
+
+	var collection = make([]interface{}, 0, cap)
+	for _, each := range ofThese {
 		for eachCollectable := range each {
-			cap += 1
+			collection = append(collection, eachCollectable)
 		}
 	}
 
-	var collection = make([]collectable, 0, cap)
-	for each := range ofThese {
-		for eachCollectable := range each {
-			collection = append(collection, collectable(eachCollectable))
-		}
-	}
-
-	return GeneralCollection{collection}
+	return GenericCollection{collection}
 }
 
 // Select of the matching collectable will be returned in one Collection
-func Select(c *Collection) (func(MapReduceFunc) Collection) {
-	return ImmutableMapReduce
+func Select(c Collection) func(FilterFunc) Collection {
+	var this = func(mrfun FilterFunc) Collection {
+		return ImmutableMapReduce(mrfun, c)
+	}
+
+	return this
 }
 
 // ImmutableMapReduce is a variant of a wellknown method
 // in effect, given a selective filter, return that which matches 'true'
-func (c Collection) ImmutableMapReduce(mrfun FilterFunc) Collection {
-	var reduction Collection
+func ImmutableMapReduce(mrfun FilterFunc, c Collection) Collection {
+	var buffer = make([]interface{}, 0, c.Len())
 	var countCollectables int = c.Len()
-	var thisHere int = 0
 
-	for thisHere < countCollectables; thisHere++ {
-		var this collectable = c.Get(thisHere)
-		if mrfun() {
+	for thisHere := 0; thisHere < countCollectables; thisHere++ {
+		var this = c.Get(thisHere)
+
+		if mrfun(this) {
 			buffer = append(buffer, this)
 		}
 	}
 
-	return c.(type){buffer}
+	return GenericCollection{buffer}
 }
 
-type GenericCollection []collectable
+// GenericCollection is a simple working example
+type GenericCollection []interface{}
 
 // Len over a GenericCollection returns length of underlying slice
 func (GC GenericCollection) Len() int {
-	return len(([]interface{})GC)
+	return len(GC)
 }
 
 // Get this element from the GeneralCollection. Note that default collectable is returned in OOB cases
-func (GC GenericCollection) Get(this int) collectable {
+func (GC GenericCollection) Get(this int) interface{} {
 	if this < 0 || this >= GC.Len() {
 		// Logging this transgression would be okay behavior.
 		// Allow out-of-bound runtime error is not okay
 		// Default value returned
-		return collectable{}
+		return nil
 	}
 	return GC[this]
 }
 
-// Must be provided with runtime safety implemented by user of library for given types
+// FilterFunc ust be provided with runtime safety implemented by user of library for given types
 type FilterFunc func(this interface{}) bool
 
-type Even(integer interface{}) bool {
+// Even will return all even integers when used to select elements in Collection
+func Even(integer interface{}) bool {
 	var whetherEven = func(isUnknown int) bool {
 		// bitmasking is by far the fastest method with unsigned integers, but we want to account for more
 		// Since I care more about the example's simplicity and less so about efficiency, we will use modulus operation instead
-		return (isUnknown % 2 == 0)
+		return (isUnknown%2 == 0)
 	}
-	switch integer.(type) {
+	switch i := integer.(type) {
 	case int, int8, int16, int32, int64:
-		return whetherEven(int(integer))
+		return whetherEven(int(i.(int)))
 	default:
 		return false
 	}
 }
 
-type Odd(integer interface{}) bool {
+// Odd this is still here
+func Odd(integer interface{}) bool {
 	return false // Implement later
 }
 
-type Short(stick interface{}) bool {
+// Short is a example of something to work on strings
+func Short(stick interface{}) bool {
 	var whetherShort bool
 	const shortie int = 5
 
 	switch stick.(type) {
 	case string:
-		whetherShort = len(stick) <= shortie
+		whetherShort = len(stick.(string)) <= shortie
 	default:
 		whetherShort = false
 	}
@@ -112,22 +117,23 @@ type Short(stick interface{}) bool {
 }
 
 var nrrTracer int = 0
-type NotReallyRandom(aldsjk interface{}) bool {
-	var trace = func() {
-		nrrTracer += 1 + nrrTracer / 23
-	}
 
-	if nrrTracer * 9 % 10 == 7 || nrrTracer * 2 % 10 < 3 {
+// NotReallyRandom sure looks random...
+func NotReallyRandom(aldsjk interface{}) bool {
+	nrrTracer += 1 + nrrTracer/23
+
+	if nrrTracer*9%10 == 7 || nrrTracer*2%10 < 3 {
 		return true
-	} else {
-		return false
 	}
+	return false
 }
 
-type All(thing interface{}) bool {
+// All or nothing, just kidding, it's all. it's EVERYTHING.
+func All(thing interface{}) bool {
 	return true
 }
 
-type None(thing interface{}) bool {
+// None shall pass!
+func None(thing interface{}) bool {
 	return false
 }
