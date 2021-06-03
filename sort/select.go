d77412230c59667f7aa002c63f049cbadcc1e93c
Gavin Kyte <gpkyte@gmail.com>
2021-06-02 21:12:16 -0400

    Play around with SelectCommonMembers
    Tests and Impl complete.
    Found a bug: `0` is not a member of any list,
    but `0` is found in the results of SelectCommonMembers in every case.

---
+++ sort/select.go
@@ -0,0 +1,98 @@
+package sort // import "github.com/GPKyte/banter/sort"
+
+type Member int
+
+func SelectCommonMembers(numLists [][]int) []int {
+	var memberLists = convertNumsToMembers(numLists)
+	// Once all memberships verified for a shared member, retain it.
+	// Basic version is a count
+
+	// Or run ReadFirstToN on the buffered input of the shared member series
+	// FirstTo(Len(memberLists))
+
+	var maxMemberships int = len(memberLists) // Once reached by MMMM, item is in every collection
+	var trackMembership = map[Member]int{}    // Must be initialized for first reference inside of trackAssistant
+	var trackAssistant = func(forThis Member) {
+		var someCount int = trackMembership[forThis] // Will default to zero if not initialized
+		trackMembership[forThis] = someCount + 1
+	}
+
+	var countUpMemberships = applyFuncToAll
+	countUpMemberships(memberLists, trackAssistant)
+
+	var commonMembers = make([]Member, 1) // Capacity...Max of the memberlists
+	var saveAllCommonMembers = applyFuncToKeysWithMatchingValues
+	var saveAnyQualifiedMember = func(m Member, count int) {
+		if count == maxMemberships {
+			commonMembers = append(commonMembers, m)
+		}
+	}
+
+	saveAllCommonMembers(trackMembership, saveAnyQualifiedMember)
+	return convertMembersToNums([][]Member{commonMembers})[0]
+}
+
+func convertNumsToMembers(src [][]int) [][]Member {
+	var conversion = make([][]Member, len(src))
+
+	for j := range src {
+		conversion[j] = make([]Member, 0, len(src[j]))
+
+		for k := range src[j] {
+			conversion[j] = append(conversion[j], Member(src[j][k]))
+		}
+	}
+	return conversion
+}
+
+func convertMembersToNums(src [][]Member) [][]int {
+	var conversion = make([][]int, len(src))
+
+	for j := range src {
+		conversion[j] = make([]int, 0, len(src[j]))
+
+		for k := range src[j] {
+			conversion[j] = append(conversion[j], int(src[j][k]))
+		}
+	}
+	return conversion
+}
+
+func applyFuncToKeysWithMatchingValues(namesAreHard map[Member]int, do func(Member, int)) {
+	for k, v := range namesAreHard {
+		do(k, v)
+	}
+}
+
+func applyFuncToAll(sliceOfSlices [][]Member, do func(Member)) {
+	for _, slice := range sliceOfSlices {
+		for _, thing := range slice {
+			do(thing)
+		}
+	}
+}
+
+func ReadFirstToN(collection []int, N int) chan int {
+	var lookup = map[int]int{}
+	var firstToN = make(chan int, len(collection))
+	var countToN = func(countThis int) bool {
+		count := lookup[countThis]
+		lookup[countThis]++
+
+		if count != N {
+			return false
+		} else {
+			return true
+		}
+	}
+	var countSort = func() {
+		for _, c := range collection {
+			if countToN(c) {
+				firstToN <- c
+			}
+		}
+		defer close(firstToN)
+	}
+	go countSort()
+	return firstToN
+}
---
+++ sort/select_test.go
@@ -0,0 +1,59 @@
+package sort_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/GPKyte/banter/sort"
+)
+
+var input = []int{7, 6, 4, 2, 5, 7, 8, 4, 2, 5, 6, 8, 9, 4, 2, 5, 8, 6, 7, 7, 9, 0, 4, 3, 2, 5, 4, 6, 5, 3, 4, 2, 3, 1, 3, 4, 7, 8, 9}
+
+func TestCountTo3(t *testing.T) {
+	var outputBuffer = []int{}
+	var sortSrc = sort.ReadFirstToN(input, 3)
+
+	for next, ok := <-sortSrc; ok; next, ok = <-sortSrc {
+		outputBuffer = append(outputBuffer, next)
+	}
+	fmt.Println("out: ", outputBuffer)
+}
+
+func TestNoCommonMembers(t *testing.T) {
+	var betterBeEmpty []int = sort.SelectCommonMembers([][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	})
+
+	if len(betterBeEmpty) > 0 {
+		t.Logf("Expected No Common Members, found %d: %v", len(betterBeEmpty), betterBeEmpty)
+		t.FailNow()
+	}
+}
+
+func TestSomeCommonMembers(t *testing.T) {
+	var justACouple []int = sort.SelectCommonMembers([][]int{
+		{1, 2, 3},
+		{1, 9, 3},
+		{1, 2, 3},
+	})
+
+	if len(justACouple) != 2 {
+		t.Logf("Expected More or Less Members, found %d: %v", len(justACouple), justACouple)
+		t.FailNow()
+	}
+}
+
+func TestAllCommonMembers(t *testing.T) {
+	var everybody []int = sort.SelectCommonMembers([][]int{
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3},
+	})
+
+	if len(everybody) != 3 {
+		t.Logf("Expected Only Common Members, found %d: %v", len(everybody), everybody)
+		t.FailNow()
+	}
+}
