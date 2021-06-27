36ea42408e3737052d03fb6b1d8d29dbeb864895
Gavin Kyte <gpkyte@gmail.com>
2021-06-27 18:17:59 -0400

    Add Test for unexported clusterTogether()
    Try new testing strategy:
    Where step-wise and unit tests for pkg function are wanted.
    Include in the same pkg.
    
    Downside? Tests are included for the exported API.
    Upside? Internals are tested in a whitebox style.
    Using a pkg_test instead would leverage the blackbox style.
    The real question? Is this a unit test? Debate GO!

---
+++ challenge/pond_unit_test.go
@@ -0,0 +1,28 @@
+package challenge
+
+import (
+	"testing"
+)
+
+func TestClusterTogether(t *testing.T) {
+	t.Fail()
+	var src = [][]int{
+		{3, 4, 4, 5, 3, 3,}, //		{e, i, i, j, f, f,},
+		{5, 2, 2, 3, 1, 3,}, //		{k, c, c, g,_a, f,},
+		{4, 1, 3, 3, 2, 6,}, //		{h, b, g, g, d, l,},
+	}
+	var batt Matrix = (*BasicMatrix)(&src)
+	var tiles = make([]Tile, len(src) * len(src[0]))
+	for dimOne, v := range src {
+		for dimTwo := range v {
+			tiles = append(tiles, Tile{rowCoordinate: dimOne, colCoordinate: dimTwo})
+		}
+	}
+
+	// Assert clusters a..l found
+	t.Log(clusterTogether(tiles, batt))
+	t.Log(clusterTogether([]Tile{
+		{rowCoordinate: 1, colCoordinate: 1,},
+		{rowCoordinate: 1, colCoordinate: 2,},
+	}, batt))
+}
