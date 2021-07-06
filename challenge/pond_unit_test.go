c3e5e7c7b4af6916bb7feb569e7b4b8bdc1ffbb7
Gavin Kyte <gpkyte@gmail.com>
2021-07-06 15:23:06 -0400

    Minor corrections on unit_test
    set init len of list to 0
    fix comma placement in literals

--- challenge/pond_unit_test.go
+++ challenge/pond_unit_test.go
@@ -7,12 +7,12 @@ import (
 func TestClusterTogether(t *testing.T) {
 	t.Fail()
 	var src = [][]int{
-		{3, 4, 4, 5, 3, 3,}, //		{e, i, i, j, f, f,},
-		{5, 2, 2, 3, 1, 3,}, //		{k, c, c, g,_a, f,},
-		{4, 1, 3, 3, 2, 6,}, //		{h, b, g, g, d, l,},
+		{3, 4, 4, 5, 3, 3}, //		{e, i, i, j, f, f,},
+		{5, 2, 2, 3, 1, 3}, //		{k, c, c, g,_a, f,},
+		{4, 1, 3, 3, 2, 6}, //		{h, b, g, g, d, l,},
 	}
 	var batt Matrix = (*BasicMatrix)(&src)
-	var tiles = make([]Tile, len(src) * len(src[0]))
+	var tiles = make([]Tile, 0, len(src)*len(src[0]))
 	for dimOne, v := range src {
 		for dimTwo := range v {
 			tiles = append(tiles, Tile{rowCoordinate: dimOne, colCoordinate: dimTwo})
@@ -22,7 +22,7 @@ func TestClusterTogether(t *testing.T) {
 	// Assert clusters a..l found
 	t.Log(clusterTogether(tiles, batt))
 	t.Log(clusterTogether([]Tile{
-		{rowCoordinate: 1, colCoordinate: 1,},
-		{rowCoordinate: 1, colCoordinate: 2,},
+		{rowCoordinate: 1, colCoordinate: 1},
+		{rowCoordinate: 1, colCoordinate: 2},
 	}, batt))
 }
