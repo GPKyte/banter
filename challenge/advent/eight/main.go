7563213aecbe5193b2c449dc22aa8ee84d788017
Gavin Kyte <gpkyte@gmail.com>
2022-12-21 00:44:14 -0500

    Add main call to treehouse pkg

---
+++ challenge/advent/eight/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/GPKyte/banter/challenge/advent/common"
+    "github.com/GPKyte/banter/challenge/advent/eight/treehouse"
+)
+
+func main() {
+    inputFile := common.OpenFirstArgAsFileReader()
+    defer inputFile.Close()
+
+    forest := treehouse.NewForest(inputFile)
+
+    fmt.Println(forest)
+    fmt.Println(forest.CountVisible())
+}
